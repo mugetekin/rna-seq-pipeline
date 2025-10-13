10be627a77b0ae5cdff3784fda1518223d90e6d6
unknown <mugetekin99@gmail.com>
2025-10-13 16:57:42 +0200

    small typo in comment line

---
+++ R/01_normalize.R
@@ -0,0 +1,19 @@
+source("R/io_helpers.R"); source("R/de_helpers.R")
+suppressPackageStartupMessages({ library(edgeR); library(limma) })
+
+cfg  <- load_cfg(); dir_prep(cfg)
+
+io   <- read_counts_annot(cfg$paths$counts)
+meta <- make_meta_from_colnames(colnames(io$counts))
+stopifnot(all(meta$SampleID == make.names(colnames(io$counts))))
+
+# do_normalize now only takes (counts, meta); filterByExpr is handled inside
+dge  <- do_normalize(io$counts, meta)
+
+fit  <- fit_limma(dge, meta)
+v    <- fit$v; fit2 <- fit$fit2
+
+saveRDS(list(annot = io$annot, meta = meta, dge = dge, v = v, fit2 = fit2),
+        file.path(cfg$paths$rds, "norm_and_fit.rds"))
+
+write_post_norm(dge, v, io$annot, cfg)
